e73270651683bcd66887c6a0e0ae564c6c8089cb
thedaemon-wizard <amon06251994@gmail.com>
2025-05-08 06:01:02 -0400

    Update Add dynamic pages in Module4

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,8 +1,7 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
 backend_url = os.getenv(
